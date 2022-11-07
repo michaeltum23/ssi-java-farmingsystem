a3354b0b37e509cc84d0aac8caccf46335b10c77
Lucipeyl <jayfiel14@gmail.com>
2022-11-07 14:38:10 +0800

    Update FarmingSystem.java

--- FarmingSystem/src/farmingsystem/FarmingSystem.java
+++ FarmingSystem/src/farmingsystem/FarmingSystem.java
@@ -14,10 +14,7 @@ public class FarmingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("World");
-        System.out.println("kevin");
-       
+ 
     }
     
 }
