0b39dea9aaccbed9e09c78224db0408663ae90f5
Jomar Tamayo <84573885+Tamayojomar@users.noreply.github.com>
2022-11-08 18:27:06 +0800

    cropsAdd

--- FarmingSystem/src/farmingsystem/controller/UserImp.java
+++ FarmingSystem/src/farmingsystem/controller/UserImp.java
@@ -7,7 +7,6 @@ package farmingsystem.controller;
 import farmingsystem.FarmingConnection;
 import farmingsystem.model.User;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
