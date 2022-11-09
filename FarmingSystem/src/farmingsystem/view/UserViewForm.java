3b3b390b4fd3c5f98d1ae9763fc050420d8cccea
Michael Tumangan <michael.tumangan23@gmail.com>
2022-11-09 10:06:36 +0800

    Rename View Form

--- FarmingSystem/src/farmingsystem/view/UserViewForm.java
+++ FarmingSystem/src/farmingsystem/view/UserViewForm.java
@@ -13,12 +13,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author MY-PC
  */
-public class ViewForm extends javax.swing.JFrame {
+public class UserViewForm extends javax.swing.JFrame {
 
     /**
      * Creates new form ViewUsers
      */
-    public ViewForm() {
+    public UserViewForm() {
         initComponents();
        Load();
        
@@ -168,21 +168,23 @@ public class ViewForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserViewForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ViewForm().setVisible(true);
+                new UserViewForm().setVisible(true);
             }
         });
     }
